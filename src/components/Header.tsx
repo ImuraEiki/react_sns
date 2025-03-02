2eadcf21a2264dbe350d4bef93f1162d000f4358
ImuraEiki <imura2005@gmail.com>
2025-03-02 12:23:45 +0900

    remove: 不要ファイル

--- src/components/Header.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export const Header = () => {
-  return (
-    <header className="bg-blue-500 text-white p-4 text-center text-lg font-bold">
-      My App
-    </header>
-  );
-};
