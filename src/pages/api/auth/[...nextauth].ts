a1957151673466d80ebbc3df5f8ff73f4bf3ead0
ImuraEiki <imura2005@gmail.com>
2025-09-07 20:10:42 +0900

    feat: ログイン時にトークンを取得

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -11,6 +11,12 @@ export default NextAuth({
       clientId: process.env.NEXT_PUBLIC_AUTH0_CLIENT_ID!,
       clientSecret: process.env.NEXT_PUBLIC_AUTH0_CLIENT_SECRET!,
       issuer: process.env.NEXT_PUBLIC_AUTH0_ISSUER!,
+      authorization: {
+        params: {
+          audience: process.env.NEXT_PUBLIC_AUTH0_AUDIENCE,
+          scope: "openid profile email"
+        },
+      }
     }),
   ],
   secret: process.env.NEXT_PUBLIC_NEXTAUTH_SECRET,
@@ -37,8 +43,18 @@ export default NextAuth({
     },
   },
   callbacks: {
-    async session({ session, token }) {
-      return session;
+    async jwt({ token, account }) {
+      if (account) {
+        token.accessToken = account.access_token;
+      }
+      return token;
+    },
+    async session({ session, token, user }) {
+      return {
+        ...session,
+        jwt: token,
+        database: user
+      };
     },
   },
 });
