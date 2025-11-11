ad852ea5a370f39c56d7d095d7b5850d0f089d96
ImuraEiki <imura2005@gmail.com>
2025-11-11 21:37:23 +0900

    test: reducerのテストをいったん削除

--- src/store/__tests__/commentSlice.test.ts
+++ src/store/__tests__/commentSlice.test.ts
@@ -1,16 +1,16 @@
-import commentReducer, { addComment } from "../commentSlice";
+// import commentReducer, { addComment } from "../commentSlice";
 
-test("addCommentを実行するとポストが追加される", () => {
-  const initialState = {
-    comments: []
-  };
-  const newState = commentReducer(initialState, addComment({
-    content: 'コメント',
-    postId: 5001,
-    userId: 502
-  }));
-  expect(newState.comments[0].id).toBe(1);
-  expect(newState.comments[0].content).toBe('コメント');
-  expect(newState.comments[0].postId).toBe(5001);
-  expect(newState.comments[0].userId).toBe(502);
-});
+// test.skip("addCommentを実行するとポストが追加される", () => {
+//   const initialState = {
+//     comments: []
+//   };
+//   const newState = commentReducer(initialState, addComment({
+//     content: 'コメント',
+//     postId: 5001,
+//     userId: 502
+//   }));
+//   expect(newState.comments[0].id).toBe(1);
+//   expect(newState.comments[0].content).toBe('コメント');
+//   expect(newState.comments[0].postId).toBe(5001);
+//   expect(newState.comments[0].userId).toBe(502);
+// });
